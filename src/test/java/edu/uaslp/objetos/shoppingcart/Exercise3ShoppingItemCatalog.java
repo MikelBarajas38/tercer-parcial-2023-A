cfb673442b791f38b3f966312c31b8fdffd57b9e
MikelBarajas38 <mikelbarajas388@gmail.com>
2023-05-08 09:50:44 -0600

    tests3 implemented

--- src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
+++ src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
@@ -2,15 +2,32 @@ package edu.uaslp.objetos.shoppingcart;
 
 import org.junit.jupiter.api.Test;
 
-public class Exercise3ShoppingItemCatalog {
+import static org.assertj.core.api.Assertions.assertThat;
 
+public class Exercise3ShoppingItemCatalog {
 
     @Test
     public void givenAValidCode_whenGetItem_thenShoppingItemIsReturned(){
+        //given
+        ShoppingItemCatalog shoppingItemCatalog = new ShoppingItemCatalog();
+
+        //when
+        ShoppingItem testItem = shoppingItemCatalog.getItem("ABC1000");
+
+        //then
+        assertThat(testItem.getCode()).isEqualTo("ABC1000");
+        assertThat(testItem.getName()).isEqualTo("Escoba");
     }
 
     @Test
     public void givenANotValidCode_whenGetItem_thenNullIsReturned(){
+        //given
+        ShoppingItemCatalog shoppingItemCatalog = new ShoppingItemCatalog();
 
+        //when
+        ShoppingItem testItem = shoppingItemCatalog.getItem("notValid");
+
+        //then
+        assertThat(testItem).isEqualTo(null);
     }
 }
